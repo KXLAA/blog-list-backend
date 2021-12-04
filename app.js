c50201b39e4deffe73eccd5e4e673b9119753d0b
Kolade Afode <kolade.afode@yahoo.com>
2021-12-04 09:26:42 +0100

    Complete exercises 4.15 - 4.16

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const config = require("./utils/config");
 const express = require("express");
 const app = express();
+require("express-async-errors");
 const cors = require("cors");
 const blogRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -27,6 +31,8 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -3,79 +3,157 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const User = require("../models/user");
 const helper = require("./test_helper");
+const bcrypt = require("bcrypt");
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
+describe("when there is initially some blogs saved", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+  });
+
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  }, 100000);
+
+  test("all blogs returned", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test("id to be defined in correct format", async () => {
+    const response = await api.get("/api/blogs");
+    const id = response.body.map((blog) => blog.id);
+    expect(id[0]).toBeDefined();
+  });
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-}, 100000);
+describe("addition of a new note", () => {
+  test("a blog can be added", async () => {
+    const newBlog = {
+      title: "The styled-components Happy Path",
+      author: "josh comeau",
+      url: "https://www.joshwcomeau.com/css/styled-components/",
+      likes: 40,
+    };
 
-test("All blogs returned", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  }, 100000);
+
+  test("like is equal to zero by default if no like property is provided", async () => {
+    const newBlog = {
+      title: "The styled-components Happy Path",
+      author: "josh comeau",
+      url: "https://www.joshwcomeau.com/css/styled-components/",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    const lastItem = blogsAtEnd[blogsAtEnd.length - 1];
+    expect(lastItem.likes).toEqual(0);
+  });
+
+  test("blog without title or url not added", async () => {
+    const newBlog = {
+      author: "josh comeau",
+      likes: 40,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length);
+  });
 });
 
-test("id to be defined", async () => {
-  const response = await api.get("/api/blogs");
-  const id = response.body.map((blog) => blog.id);
-  expect(id[0]).toBeDefined();
-});
+describe("when there is initially one user in db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
 
-test("a blog can be added", async () => {
-  const newBlog = {
-    title: "The styled-components Happy Path",
-    author: "josh comeau",
-    url: "https://www.joshwcomeau.com/css/styled-components/",
-    likes: 40,
-  };
+    const passwordHash = await bcrypt.hash("testPassword", 10);
+    const user = new User({ username: "root", passwordHash });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await user.save();
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-});
+  test("creation of new user", async () => {
+    const usersAtStart = await helper.usersInDb();
 
-test("like is equal to zero", async () => {
-  const newBlog = {
-    title: "The styled-components Happy Path",
-    author: "josh comeau",
-    url: "https://www.joshwcomeau.com/css/styled-components/",
-  };
+    const newUser = {
+      username: "kxlaa",
+      name: "Tooka Lore",
+      password: "hfhhsuhhuu2WDD",
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const lastItem = blogsAtEnd[blogsAtEnd.length - 1];
-  expect(lastItem.likes).toEqual(0);
-});
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
 
-test("blog without title or url not added", async () => {
-  const newBlog = {
-    author: "josh comeau",
-    likes: 40,
-  };
+    const usernames = usersAtEnd.map((user) => user.username);
+    expect(usernames).toContain(newUser.username);
+  });
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  test("creation of new user fails with proper statuscode and message if username already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Manny Wellz",
+      password: "444rrrttt666",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    console.log(result.body.error);
+    expect(result.body.error).toContain("`username` to be unique");
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  }, 100000);
+
+  test("creation fails if password is too short", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "tupple",
+      name: "Manny weinger",
+      password: "456",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("password too short");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  }, 100000);
 });
 
 afterAll(() => {
